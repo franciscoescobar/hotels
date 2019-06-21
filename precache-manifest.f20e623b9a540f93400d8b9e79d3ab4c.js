4460c1ed62e23edb5fa127f4d2f19377bcbdd291
franciscoescobar <franciscoescobar@users.noreply.github.com>
2019-06-21 10:29:43 -0300

    Updates

---
+++ precache-manifest.f20e623b9a540f93400d8b9e79d3ab4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b689929180d8e7af4a97aa162d3624f",
+    "url": "/hotels/index.html"
+  },
+  {
+    "revision": "308a01239d54982b66c5",
+    "url": "/hotels/static/css/2.21555de0.chunk.css"
+  },
+  {
+    "revision": "000b64322f92d1bf454c",
+    "url": "/hotels/static/css/main.564b6524.chunk.css"
+  },
+  {
+    "revision": "308a01239d54982b66c5",
+    "url": "/hotels/static/js/2.dc98e4fa.chunk.js"
+  },
+  {
+    "revision": "000b64322f92d1bf454c",
+    "url": "/hotels/static/js/main.990603b5.chunk.js"
+  },
+  {
+    "revision": "6ce7b40ee3d75d1d4af8",
+    "url": "/hotels/static/js/runtime~main.17b45294.js"
+  }
+]);
